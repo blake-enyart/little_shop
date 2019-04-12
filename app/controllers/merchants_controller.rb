13c399ed65fb1b3a2d1b99ed7235e63a401d99ad
Blake <blake.enyart@gmail.com>
2019-04-12 11:40:44 -0600

    Edit merchant index view and controller to integrate top sellers

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -11,5 +11,6 @@ class MerchantsController < ApplicationController
     @top_states_by_order_count = User.top_user_states_by_order_count(3)
     @top_cities_by_order_count = User.top_user_cities_by_order_count(3)
     @top_orders_by_items_shipped = Order.sorted_by_items_shipped(3)
+    @top_ten_sellers_current = User.top_selling_merchants_current(10)
   end
 end
